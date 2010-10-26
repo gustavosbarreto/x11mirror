549097a4de3364a06ad170ff91db7a41a9efda91
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2010-10-26 11:39:21 -0200

    Working

--- test/mirror.cpp
+++ test/mirror.cpp
@@ -1,6 +1,7 @@
 #include "mirror.hpp"
 
 #include <QApplication>
+#include <QPainter>
 #include <QDebug>
 
 #include "../x11mirror.hpp"
@@ -8,5 +9,12 @@
 Mirror::Mirror()
     : QWidget()
 {
-    new X11Mirror(qApp->arguments().at(1).toULong(0, 16), this);
+    x11Mirror = new X11Mirror(qApp->arguments().at(1).toULong(0, 16), this);
+    connect(x11Mirror, SIGNAL(updated()), SLOT(repaint()));
+}
+
+void Mirror::paintEvent(QPaintEvent *e)
+{
+    QPainter painter(this);
+    painter.drawPixmap(0, 0, x11Mirror->buffer());
 }
--- test/mirror.hpp
+++ test/mirror.hpp
@@ -2,12 +2,20 @@
 
 #include <QWidget>
 
+class X11Mirror;
+
 class Mirror: public QWidget
 {
     Q_OBJECT
 
 public:
     Mirror();
+
+protected:
+    virtual void paintEvent(QPaintEvent *e);
+
+private:
+    X11Mirror *x11Mirror;
 };
 
 #endif
