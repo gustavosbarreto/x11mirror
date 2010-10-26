550f97794b241a66952fffe16489254ff8bd8a4d
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2010-10-26 11:11:50 -0200

    Fixes the test app

--- test/mirror.cpp
+++ test/mirror.cpp
@@ -1,6 +1,11 @@
 #include "mirror.hpp"
+#include "../x11mirror.hpp"
+
+#include <QApplication>
+#include <QDebug>
 
 Mirror::Mirror()
     : QWidget()
 {
+    new X11Mirror(qApp->arguments().at(1).toULong(0, 16), this);
 }
